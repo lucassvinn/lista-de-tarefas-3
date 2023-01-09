a44e456af7ad81b1bbd9869b024d4a5bce893645
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-01-09 19:28:30 -0300

    Add files via upload

---
+++ main (18).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int z;
+    
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &i);
+    
+    for (z=0; z<=i; z++)
+        printf("%d\n", z);
+
+    return 0;
+}
+
+
