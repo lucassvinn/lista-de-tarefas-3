1e3a111332f5fdbd2c44df3feb0b93e590bf6f27
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-01-10 17:56:25 -0300

    Update main (21).c

--- main (21).c
+++ main (21).c
@@ -7,6 +7,6 @@ int main()
     for (letras[0]; letras[0]<=letras[2]; letras[0]++) {
         printf("%c\n", letras[0]);
     }
-    
+    // Apenas um teste de código...estudando o for de outras maneiras
     return 0;
 }
