132ccbb0623f72eac0d88c8f3d17db14bffb8eec
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-01-10 17:55:15 -0300

    Add files via upload

---
+++ main (21).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char letras[3] = {'A', 'B', 'C'};
+    
+    for (letras[0]; letras[0]<=letras[2]; letras[0]++) {
+        printf("%c\n", letras[0]);
+    }
+    
+    return 0;
+}
